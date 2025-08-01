4dff5f9adc22602c2c38f7407750dea82a1dbde8
Anwar Reghai <reganwar36@gmail.com>
2025-08-01 02:44:41 +0100

    source file

---
+++ Source/Vr_football/Vr_football.cpp
@@ -0,0 +1,6 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#include "Vr_football.h"
+#include "Modules/ModuleManager.h"
+
+IMPLEMENT_PRIMARY_GAME_MODULE( FDefaultGameModuleImpl, Vr_football, "Vr_football" );
